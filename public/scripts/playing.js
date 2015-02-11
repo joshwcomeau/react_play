06a834cec8a486a45c3ef0f8c16a901ee325b650
Joshua Comeau <joshwcomeau@gmail.com>
2015-02-11 15:58:08 -0500

    Created the DotForm React object. Hooked up the form submission to its associated method.

--- public/scripts/playing.js
+++ public/scripts/playing.js
@@ -1,17 +1,45 @@
+var Playground = React.createClass({
+  render: function() {
+    return (
+      <div className="playground">
+        <Dot width={200} height={200} />
+        <DotForm />
+      </div>
+    );
+  }
+});
+
 var Dot = React.createClass({
   render: function() {
     var dotStyles = {
       width:  this.props.width,
       height: this.props.height
     }
-    console.log(dotStyles)
+
     return (
       <div className="dot" style={dotStyles}></div>
     );
   }
 });
 
+var DotForm = React.createClass({
+  updateDot: function(e) {
+    e.preventDefault();
+    // placeholder behavior
+    alert("Updated!");
+  },
+  render: function() {
+    return (
+      <form className="dotForm" onSubmit={this.updateDot}>
+        <input type="text" placeholder="width" /> x <input type="text" placeholder="height" />
+        <input type="submit" value="Update The Dot!" />
+      </form>
+    );
+  }
+});
+
+
 React.render(
-  <Dot width={200} height={200} />,
+  <Playground />,
   document.getElementById('content')
 );
