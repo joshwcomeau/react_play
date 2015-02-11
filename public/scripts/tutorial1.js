e417bc8547c16fc7017c319ad4a1716c28377166
Joshua Comeau <joshwcomeau@gmail.com>
2015-02-11 15:07:03 -0500

    Added polling so that changes to the JSON file can be detected.

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -8,11 +8,30 @@ var data = [
 var converter = new Showdown.converter();
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return { data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -61,6 +80,6 @@ var Comment = React.createClass({
 })
 
 React.render(
-  <CommentBox data={data} />,
+  <CommentBox url="comments.json" pollInterval={2000} />,
   document.getElementById('content')
 );
